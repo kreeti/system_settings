48c47bc9215968c623d2caa9a0ebb15c5f289424
Surendra Singhi <ssinghi@kreeti.com>
2018-07-14 23:16:31 +0530

    updated emacs settings

--- emacs24.el
+++ emacs24.el
@@ -20,27 +20,60 @@
 (setq require-final-newline t)
 (setq next-line-add-newlines nil)
 (setq delete-trailing-lines t)
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  ;;(add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
 (setq css-indent-offset 2)
 
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
-(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
 
 ;;from manage package options you can install various packages like
-;; * auto-complete mode
+;; * company-mode
 ;; * ruby mode
 ;; * scss-mode
-;; * js3-mode
 ;; * json-mode
 ;; * csv-mode
 ;; * yaml-mode
 ;; * flymake-ruby -- flymake is for syntax checking on the go
+;; * rjsx-mode -- react files
+;; * js2-mode -- ES6 mode
+;; * js2-refactor - ES mode
+;; * rubocopy
+;; * flycheck
 ;; etc
+;;
+;; Install eslint for flychecker
+;; sudo npm install eslint -g
+;;
 
-(add-hook 'ruby-mode-hook 'auto-complete-mode)
+;; use rjsx-mode for .js files
+(add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'rjsx-mode)
+(flycheck-add-mode 'javascript-eslint 'js2-mode)
+
+;; turn on company mode globally
+(add-hook 'after-init-hook 'global-company-mode)
